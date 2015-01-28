66fb92c58cbacdacc1edd364aae04ddd0fcc7b43
Manuel Suárez <man.srz@gmail.com>
2015-01-28 01:03:52 -0500

    Add first view and template

--- kokoaCalendar/webapp/views.py
+++ kokoaCalendar/webapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def horario(request):
+    #user = request.user
+    materias_disponibles =['Calculo','Historia','Lenguaje'] 
+    horarios = []
+    context = {
+      'materias':materias_disponibles
+    }
+    template = 'web/horario.html'
+    return render(request, template, context)
+
+
